7e8cb01d7c9c6cbda10571452ef3e0bb753a13a0
Mark Needham <m.h.needham@gmail.com>
2013-06-30 20:06:54 +0100

    pull submit override into Stadiums

--- public/football-stadiums.js
+++ public/football-stadiums.js
@@ -42,36 +42,5 @@ $(document).ready(function() {
 		stadiums.update();
 	});
 
-	$("#stadium-search").submit(function(e) {
-		e.preventDefault();
-		var lat = $("#inputLatLong").val().split(",")[0].trim(); 
-		var lon = $("#inputLatLong").val().split(",")[1].trim(); 
-		var distance = $("#inputDistance").val();
-
-		var result = "<table class=\"table\"><thead>";
-		result += "<tr><th>Team</th><th>Stadium</th><th>Distance (km)</th></tr>";
-		result += "</thead><tbody>";		
-
-		$.getJSON('/stadiums/' + lat + "/" + lon + "/" + distance, function(data) {
-			$.each(data, function(key, val) {
-				result += "<tr>";
-				result += "<td>" + val.team + "</td>";
-				result += "<td><a target='_blank' href='https://maps.google.co.uk/maps?q=" + val.lat + "," + val.lon + "'>" + val.stadium + "</a></tf>";
-				result += "<td>" + val.distance + "</td>";
-				result += "</tr>";				
-			});
-
-			if(data.length == 0) {
-				result += "<td colspan='3'>No stadiums found - try changing location or distance</td>";
-			}
-
-			result += "</tbody></table>";			
-
-			$("div.results").html(result)
-		});
-
-		return false;		
-	});
-
 	stadiums.update();
 });
--- public/stadiums.js
+++ public/stadiums.js
@@ -1,4 +1,36 @@
 var Stadiums = function(formElement) {
+	var NO_STADIUMS_MESSAGE = "No stadiums found - try changing location or distance";
+	formElement.submit(function(e) {
+		e.preventDefault();
+		var lat = $("#inputLatLong").val().split(",")[0].trim(); 
+		var lon = $("#inputLatLong").val().split(",")[1].trim(); 
+		var distance = $("#inputDistance").val();
+
+		var result = "<table class=\"table\"><thead>";
+		result += "<tr><th>Team</th><th>Stadium</th><th>Distance (km)</th></tr>";
+		result += "</thead><tbody>";		
+
+		$.getJSON('/stadiums/' + lat + "/" + lon + "/" + distance, function(data) {
+			$.each(data, function(key, val) {
+				result += "<tr>";
+				result += "<td>" + val.team + "</td>";
+				result += "<td><a target='_blank' href='https://maps.google.co.uk/maps?q=" + val.lat + "," + val.lon + "'>" + val.stadium + "</a></tf>";
+				result += "<td>" + val.distance + "</td>";
+				result += "</tr>";				
+			});
+
+			if(data.length == 0) {
+				result += "<td colspan='3'>" + NO_STADIUMS_MESSAGE + "</td>";
+			}
+
+			result += "</tbody></table>";			
+
+			$("div.results").html(result)
+		});
+
+		return false;		
+	});
+
 	var obj = {
 		update : function() {
 			formElement.submit();
